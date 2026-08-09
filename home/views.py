4dec6449017bc59f707afa358c7c230ef8dd0c7d
Mojtaba Charchian <M.Charchian@Sony.Net>
2026-08-09 17:27:14 +0330

    todo added

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from todo.models import Todo
 
 # Create your views here.
 
 def index_page(request):
-    context = {}
+    context = {
+        'todos':Todo.objects.order_by('priority').all()
+    }
     return render(request,'home/index.html',context)
