272e5bb9a878db925aee72e6af8973e9e95a508c
Mojtaba Charchian <M.Charchian@Sony.Net>
2026-08-11 11:03:27 +0330

    gat all todos with serializer

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path('',include('home.urls'))
+    path('',include('home.urls')),
+    path('todo/',include('todo.urls'))
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .serializers import TodoSerializer
+from .models import Todo
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def get_all_todos(request:Request):
+    todos = Todo.objects.order_by('priority').all()
+    todo_serializer = TodoSerializer(todos,many=True)
+    return Response(todo_serializer.data,status.HTTP_200_OK)
+
